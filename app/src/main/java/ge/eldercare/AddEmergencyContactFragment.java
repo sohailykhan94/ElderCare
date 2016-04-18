66d291d1abb92e15d9ee169ad630daa2c68533df
Sohail Yar Khan <sohailykhan94@gmail.com>
2016-04-18 19:20:11 +0800

    Initial Commit

---
+++ app/src/main/java/ge/eldercare/AddEmergencyContactFragment.java
@@ -0,0 +1,72 @@
+package ge.eldercare;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v4.app.DialogFragment;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AddEmergencyContactFragment extends DialogFragment {
+
+
+    public AddEmergencyContactFragment() {
+        // Empty constructor required for DialogFragment
+    }
+
+    public interface EmergencyDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+    }
+
+    EmergencyDialogListener mListener;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (EmergencyDialogListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.alert_emergency, null))
+                // Add action buttons
+                .setPositiveButton("ADD", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        mListener.onDialogPositiveClick(AddEmergencyContactFragment.this);
+
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
---
+++ app/src/main/java/ge/eldercare/LoginActivity2.java
@@ -0,0 +1,170 @@
+package ge.eldercare;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.DialogFragment;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity2 extends AppCompatActivity implements AddEmergencyContactFragment.EmergencyDialogListener{
+
+    /**
+     * Id to identity READ_CONTACTS permission request.
+     */
+    private static final int REQUEST_READ_CONTACTS = 0;
+
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+
+
+    Context context;
+
+    String m_Text;
+
+    // UI references.
+    private View mProgressView;
+    private View mLoginFormView;
+
+    EditText name;
+    EditText phone;
+    TextView contact1;
+    TextView contact2;
+    RadioGroup radioGroupTitle;
+    RadioButton radioButtonTitle;
+    ArrayList<String> contactListNames = new ArrayList<>();
+    ArrayList<String> contactListPhone = new ArrayList<>();
+    int contactCount = 0;
+    MultiAutoCompleteTextView allergies;
+    MultiAutoCompleteTextView diseases;
+    String[] allergiesList={"Celery ","Gluten","Crustaceans","Eggs","Fish","Lupin","Milk", "Molluscs", "Mustard", "Nuts", "Peanuts", "Sesame seeds", "Soya", "Sulphur dioxide"};
+    String[] diseaseList={"Arthritis ","Heart Disease","Cancer","Respiratory Diseases","Alzheimer’s Disease","Osteoporosis","Diabetes", "Influenza and Pneumonia", "Substance Abuse", "Obesity", "Depression", "Oral Health", "Shingles", "Septicaemia", "Dementia"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context  = this.getApplicationContext();
+        setContentView(R.layout.activity_login2);
+        // Set up the form.
+        contact1 = (TextView) findViewById(R.id.contact1);
+        contact2 = (TextView) findViewById(R.id.contact2);
+        allergies=(MultiAutoCompleteTextView)findViewById(R.id.allergies);
+        diseases=(MultiAutoCompleteTextView)findViewById(R.id.diseases);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,allergiesList);
+        ArrayAdapter adapter2 = new ArrayAdapter(this,android.R.layout.simple_list_item_1,diseaseList);
+
+        allergies.setAdapter(adapter);
+        allergies.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+
+        diseases.setAdapter(adapter2);
+        diseases.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+    }
+
+    public void addEmergencyContact(View v){
+
+        if(contactCount == 2){
+            Toast.makeText(this, "Hey, you can add a maximum of two contacts" , Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        FragmentManager fm = getSupportFragmentManager();
+        AddEmergencyContactFragment editNameDialog = new AddEmergencyContactFragment();
+        editNameDialog.show(fm, "fragment_edit_name");
+
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        // User touched the dialog's positive button
+        name = (EditText) dialog.getDialog().findViewById(R.id.emergencyName);
+        phone = (EditText) dialog.getDialog().findViewById(R.id.emergencyNumber);
+        radioGroupTitle = (RadioGroup) dialog.getDialog().findViewById(R.id.radioTitle);
+
+        int selectedId = radioGroupTitle.getCheckedRadioButtonId();
+
+        // find the radiobutton by returned id
+        radioButtonTitle = (RadioButton) dialog.getDialog().findViewById(selectedId);
+
+        String title = radioButtonTitle.getText().toString();
+
+        if(contactCount == 2){
+            Toast.makeText(this, "Hey, you can add a maximum of two contacts" , Toast.LENGTH_LONG).show();
+            return;
+        }else if(contactCount ==1 ){
+            contactListNames.add(name.getText().toString());
+            contactListPhone.add(phone.getText().toString());
+            contact2.setText(title + " " + name.getText());
+            contactCount++;
+        }else{
+            contactListNames.add(name.getText().toString());
+            contactListPhone.add(phone.getText().toString());
+            contact1.setText(title + " " + name.getText());
+            contactCount++;
+        }
+    }
+
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+}
+
